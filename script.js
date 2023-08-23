2a3f01d6e1e55328179fe80c0889e4f3d265b3f3
AlexDIce <49554882+AlexDice@users.noreply.github>
2023-08-23 16:16:30 +0300

    Accounted for other choice on player's choice function

--- script.js
+++ script.js
@@ -3,25 +3,41 @@ function getRandomNum(){
     /* no + 1 e.g.: "*3) + 1" to make it 1-3 bc array counts from 0*/
     return Math.floor(Math.random()*3);
 }
+
 /*Computer choice of RPS */
 function getComputerChoice (){
-    let listoptions = ["Rock","Paper","Scisscors"]
-    return listoptions[getRandomNum()]
+    let compList = ["rock","paper","scisscors"]
+    return compList[getRandomNum()]
 }
-/* Player's Selection function */
-function playerSelection (){
+
+/* Player's Selection function, No choise for player to decide to choose again */
+function getPlayerSelection (){
     let playerChoice = prompt("Choose: Rock, Paper, Scissors")
+    if (null===playerChoice || undefined === playerChoice || ""===playerChoice){
+        window.alert("You don't wanna play? :<")
+        return
+    }else{
     playerChoice = playerChoice.toLowerCase()
+}
     if ("rock"===playerChoice || "paper"===playerChoice || "scissors"===playerChoice){
-    return playerChoice}
-    else{
+    return playerChoice
+    } else{
         window.alert("You can only choose: Rock , Paper or Scisscors")
     }
 }
-playerSelection ()
+
+const PlayerSelection = getPlayerSelection ()
 const computerSelection = getComputerChoice()
+console.log (PlayerSelection)
+console.log (computerSelection)
 
 function gameRounds(playerSelection, computerSelection){
+    if(null===playerSelection || undefined === playerSelection || ""===playerChoice){
+        return
+    }
+
+    
+
 
 }
 
