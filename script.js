cfdc905fc01bf51e619c5ffa66d14d4f60675367
AlexDIce <49554882+AlexDice@users.noreply.github>
2023-08-23 15:47:50 +0300

    Player's choice complete

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 console.log("hello world")
 function getRandomNum(){
-    /* no + 1 e.g.: "*3) + 1" to make it 1-3 bc array count from 0*/
+    /* no + 1 e.g.: "*3) + 1" to make it 1-3 bc array counts from 0*/
     return Math.floor(Math.random()*3);
 }
-
+/*Computer choice of RPS */
 function getComputerChoice (){
     let listoptions = ["Rock","Paper","Scisscors"]
     return listoptions[getRandomNum()]
 }
-
-const playerSelection = "rock";
+/* Player's Selection function */
+function playerSelection (){
+    let playerChoice = prompt("Choose: Rock, Paper, Scissors")
+    playerChoice = playerChoice.toLowerCase()
+    if ("rock"===playerChoice || "paper"===playerChoice || "scissors"===playerChoice){
+    return playerChoice}
+    else{
+        window.alert("You can only choose: Rock , Paper or Scisscors")
+    }
+}
+playerSelection ()
 const computerSelection = getComputerChoice()
 
 function gameRounds(playerSelection, computerSelection){
